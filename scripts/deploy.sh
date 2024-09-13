774902d24e3fd24f0e3dd52b30f33cf8e916764f
runnz121 <runnz121@gmail.com>
2024-09-13 23:10:50 +0900

    [feat] deploy sh build path 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-REPOSITORY=/home/ubuntu/kitten-api
+REPOSITORY=/home/ubuntu/kitten-api/kitten-api
 # shellcheck disable=SC2164
 cd $REPOSITORY
 
@@ -24,5 +24,5 @@ sudo killall java
 echo "> $JAR_PATH start"
 #sudo nohup java -jar /home/ubuntu/deploy_test/build/libs/deploy-0.0.1-SNAPSHOT.jar
 #sudo nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null
-sudo nohup java -jar /home/ubuntu/kitten-api/build/libs/kitten-api-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+sudo nohup java -jar /home/ubuntu/kitten-api/kitten-api/build/libs/kitten-api-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 #sudo nohup java -jar /home/ubuntu/deploy_test/build/libs/deploy-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null
