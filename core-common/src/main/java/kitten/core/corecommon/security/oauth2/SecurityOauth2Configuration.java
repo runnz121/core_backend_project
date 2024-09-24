7e704b1f9cc4a08dd40057cdf4bdcce5b9206db4
runnz121 <runnz121@gmail.com>
2024-09-25 00:16:54 +0900

    [feat] 시큐리티 설정 수정2

--- core-common/src/main/java/kitten/core/corecommon/security/oauth2/SecurityOauth2Configuration.java
+++ core-common/src/main/java/kitten/core/corecommon/security/oauth2/SecurityOauth2Configuration.java
@@ -72,7 +72,8 @@ public class SecurityOauth2Configuration {
         http
                 .authorizeHttpRequests(
                         authorize -> authorize
-                                .requestMatchers("/actuator/**", "/oauth2/**", "/**").permitAll()
+                                .requestMatchers("/**", "/actuator/**").permitAll()
+                                .anyRequest().permitAll()
 //                                .anyRequest().authenticated()
                                 // 임시로 모두 허용
 //                                .requestMatchers("/**").permitAll()
