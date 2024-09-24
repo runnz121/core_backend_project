fe3d14d2af93ddb1d9a541bfbdf8db33fce4111d
runnz121 <runnz121@gmail.com>
2024-09-25 01:28:13 +0900

    [feat] 테스트를 위한 시큐리티 설정

--- core-common/src/main/java/kitten/core/corecommon/security/filter/CustomSecurityConfiguration.java
+++ core-common/src/main/java/kitten/core/corecommon/security/filter/CustomSecurityConfiguration.java
@@ -1,17 +1,39 @@
 package kitten.core.corecommon.security.filter;
 
+import kitten.core.corecommon.security.oauth2.Oauth2AccessDeniedHandler;
+import kitten.core.corecommon.security.oauth2.Oauth2CustomUserService;
+import kitten.core.corecommon.security.oauth2.Oauth2EntryPointHandler;
+import kitten.core.corecommon.security.oauth2.Oauth2SuccessHandler;
+import kitten.core.corecommon.security.service.TokenService;
+import kitten.core.corecommon.security.service.UserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
+@RequiredArgsConstructor
 public class CustomSecurityConfiguration {
+
+    private final ApplicationEventPublisher publisher;
+
+    private final Oauth2SuccessHandler oauth2SuccessHandler;
+
+    private final UserDetailService userDetailService;
+    private final TokenService tokenService;
 
     @Description("이 설정은 스프링 시큐리티 필터 체인을 타지 않는다")
     @Bean
@@ -27,6 +49,35 @@ public class CustomSecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(corsConfigurer -> corsConfigurer.configurationSource(configurationSource))
                 .sessionManagement(sessionConfigurer -> sessionConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http
+                .oauth2Login(
+                        oauth2 -> oauth2
+                                .authorizationEndpoint(
+                                        endpoint -> endpoint.baseUri("/oauth2/authorize")
+                                )
+                                .redirectionEndpoint(
+                                        redirect -> redirect.baseUri("/oauth2/callback/**")
+                                )
+                                .userInfoEndpoint(
+                                        userInfo -> userInfo.userService(new Oauth2CustomUserService(publisher)))
+                                .successHandler(oauth2SuccessHandler)
+                );
+        http
+                .addFilterBefore(new AuthorizationFilter(authenticationManager(userDetailService, passwordEncoder()), userDetailService, tokenService), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailService userDetailsService,
+                                                       PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
