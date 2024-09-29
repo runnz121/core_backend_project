1014b4e6c51389e2089322cc4f54e6d8c2ee5069
runnz121 <runnz121@gmail.com>
2024-09-29 16:10:02 +0900

    [feat] custom filter 에 oauth 설정

--- core-common/src/main/java/kitten/core/corecommon/security/filter/CustomSecurityConfiguration.java
+++ core-common/src/main/java/kitten/core/corecommon/security/filter/CustomSecurityConfiguration.java
@@ -1,5 +1,10 @@
 package kitten.core.corecommon.security.filter;
 
+import kitten.core.corecommon.security.oauth2.Oauth2AuthorizationRequestRepository;
+import kitten.core.corecommon.security.oauth2.Oauth2CustomUserService;
+import kitten.core.corecommon.security.oauth2.Oauth2SuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
@@ -11,7 +16,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
+@RequiredArgsConstructor
 public class CustomSecurityConfiguration {
+
+    private final ApplicationEventPublisher publisher;
+    private final Oauth2SuccessHandler oauth2SuccessHandler;
+    private final Oauth2AuthorizationRequestRepository oauth2AuthorizationRequestRepository;
 
     @Description("이 설정은 스프링 시큐리티 필터 체인을 타지 않는다")
     @Bean
@@ -19,16 +29,29 @@ public class CustomSecurityConfiguration {
     public SecurityFilterChain permitAllFilterChain(HttpSecurity http,
                                                     CorsConfigurationSource configurationSource) throws Exception {
         http
-//                .securityMatcher(request ->
-//                        !request.getRequestURI().contains("oauth2")
-//                                || !request.getRequestURI().contains("auth")
-//                )
-//                .authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll())
+                .securityMatcher("/favicon.ico")
+                .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/oauth2/**").permitAll())
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .cors(corsConfigurer -> corsConfigurer.configurationSource(configurationSource))
                 .sessionManagement(sessionConfigurer -> sessionConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .oauth2Login(
+                        oauth2 -> oauth2
+                                .authorizationEndpoint(
+                                        endpoint -> endpoint.baseUri("/oauth2/authorize/**")
+                                                .authorizationRequestRepository(oauth2AuthorizationRequestRepository)
+                                )
+                                .redirectionEndpoint(
+                                        redirect -> redirect.baseUri("/oauth2/callback/**")
+                                )
+                                .userInfoEndpoint(
+                                        userInfo -> userInfo.userService(new Oauth2CustomUserService(publisher))
+                                )
+                                .successHandler(oauth2SuccessHandler)
+                );
         return http.build();
     }
 }
