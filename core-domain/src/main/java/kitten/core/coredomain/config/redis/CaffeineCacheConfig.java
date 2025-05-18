a512869c2d9ada39815f3c308a63b9ff71b78b4b
runnz121 <runnz121@gmail.com>
2025-05-18 17:33:58 +0900

    [feat] redis 설정추가 (글로벌, 카페인)

---
+++ core-domain/src/main/java/kitten/core/coredomain/config/redis/CaffeineCacheConfig.java
@@ -0,0 +1,33 @@
+package kitten.core.coredomain.config.redis;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CaffeineCacheConfig {
+
+    private static final String ADS_CAFFEINE_CACHE = "boardCaffeineCache";
+
+    @Bean
+    @Primary
+    public CacheManager cacheManager() {
+
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager(ADS_CAFFEINE_CACHE);
+
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(500)
+                .expireAfterWrite(Duration.ofMinutes(10))
+                .recordStats());
+
+        return cacheManager;
+    }
+}
---
+++ core-domain/src/main/java/kitten/core/coredomain/config/redis/GlobalRedisConfig.java
@@ -0,0 +1,29 @@
+package kitten.core.coredomain.config.redis;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GlobalRedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedissonClient redissonClient() {
+
+        RedissonClient redisson = null;
+        Config config = new Config();
+        config.useSingleServer().setAddress(redisHost + ":" + redisPort);
+        redisson = Redisson.create(config);
+
+        return redisson;
+    }
+}
